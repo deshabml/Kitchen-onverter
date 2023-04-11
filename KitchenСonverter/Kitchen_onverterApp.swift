4773e7f02a6bd7e20878260401572cac7eba9884
deshabml <deshab@yandex.ru>
2023-04-11 08:53:30 +0300

    Initial Commit

---
+++ KitchenСonverter/Kitchen_onverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitchen_onverterApp.swift
+//  KitchenСonverter
+//
+//  Created by Лаборатория on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_onverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
