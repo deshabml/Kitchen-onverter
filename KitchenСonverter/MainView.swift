d4037659381cd8e58d8d91d8927156dcd51fceeb
deshabml <deshab@yandex.ru>
2023-04-11 12:51:19 +0300

    Add first Vstack for text productQuantity

--- KitchenСonverter/MainView.swift
+++ KitchenСonverter/MainView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var productQuantity: String = ""
+
     var body: some View {
         VStack {
+            VStack(alignment: .center,
+                   spacing: 16) {
+                TextField("Количество", text: $productQuantity)
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(18)
+                    .keyboardType(.numberPad)
+                Text("0,000000")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.5))
+                    .cornerRadius(18)
+            }
+            .padding()
 
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
